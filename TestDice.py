0fe5a62a82936bb9b1bb1f1b6b93ed5adff3c08c
Frida Johannesson <fridasanneamanda@gmail.com>
2024-03-04 21:04:50 +0100

    testing coverage

--- TestDice.py
+++ TestDice.py
@@ -1,17 +1,15 @@
 import unittest
-import Dice
+from Dice import Dice
 
 
 class TestDiceClass(unittest.TestCase):
     """Test the Dice class"""
-
-
-def test_rollTheDice(self):
-    """Test the rollTheDice method"""
-    dice = Dice(6)
-    result = dice.rollTheDice()
-    self.assertGreaterEqual(result, 1)
-    self.assertLessEqual(result, 6)
+    def test_rollTheDice(self):
+        """Test the rollTheDice method"""
+        dice = Dice(6)
+        result = dice.rollTheDice()
+        self.assertGreaterEqual(result, 1)
+        self.assertLessEqual(result, 6)
 
 
 if __name__ == "__main__":
