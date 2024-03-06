82eb9086df96c57f8f97cb340a92f14d48ca48ed
Merjam Farj Al-Beibani0015 <merjam.al-beibani0015@stud.hkr.se>
2024-03-06 17:45:43 +0100

    new changes

--- src/Pig/main.py
+++ src/Pig/main.py
@@ -1,12 +1,7 @@
 def main():
-    import cmd
-<<<<<<< HEAD
+
+    import cmd  
     from Intelligence import Intelligence
-    from Dice import Dice
-    from Rules import Rules
-    from DiceHand import DiceHand
-=======
->>>>>>> FriddeFrid_branch
     from Player import Player
     from Game import Game
     from Rules import Rules
@@ -23,38 +18,17 @@ def main():
         print("* 5. Quit               *")
         print("*************************")
 
-<<<<<<< HEAD
-        playersName2 = input("Please enter your name (Player 2): ")
-        print(f"You entered: {playersName2}")
-        player2 = Player("Player 2")
-        player2.score = 0
-        player2.createPlayer(playersName2)
-        dictOfPlayers[playersName2] = 0
-        dictOfPlayers[player2.name] = player2.score
-        print(dictOfPlayers.values())
-
-        option = int(input("Please enter a option: \n"))
-
-        while True:
-            print(">> Player vs player <<\n")
-=======
         try:
             option = int(input("Please enter an option: "))
             
->>>>>>> FriddeFrid_branch
             if option == 1:
                 print(">> Player vs Computer <<\n")
+                Intelligence = Intelligence()
+                Intelligence.intelligence_move()
+                Intelligence.move()
                 # Implement one-player mode here
 
             elif option == 2:
-<<<<<<< HEAD
-                print(">> Player vs computer <<\n")
-                Intelligence.intelligence_move()
-                Intelligence.move()
-                rolltheDice = int(input("Players turn - please enter 0 to roll"
-                                        + " the dice. "))
-                
-=======
                 print(">> Player vs Player <<\n")
                 playersName1 = input("Please enter your name (Player 1): ")
                 player1 = Player(playersName1)
@@ -62,7 +36,6 @@ def main():
                 player2 = Player(playersName2)
                 newGame = Game()
                 newGame.PlayerVsPlayer(player1, player2, listOfPoints)
->>>>>>> FriddeFrid_branch
                 
             elif option == 3:
                 print(">> Highscore <<\n")
@@ -82,7 +55,6 @@ def main():
 
         except ValueError:
             print("Invalid input. Please enter a number.")
-
 
 if __name__ == "__main__":
     main()
