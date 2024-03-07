7b746a009836dcec7eaf1ccf7ec1c5d5d01be87a
Frida Johannesson <fridasanneamanda@gmail.com>
2024-03-07 21:34:09 +0100

    Still trying with the highscore

--- src/Pig/HighScore.py
+++ src/Pig/HighScore.py
@@ -12,7 +12,27 @@ class HighScore:
 
     def saveScore(self, dictOfScores):
         """Save the high score to a file."""
-        self.scores.update(dictOfScores)
+        highScoreDict = dictOfScores
+        player1, player2 = list(highScoreDict.keys())
+        score1, score2 = list(highScoreDict.values())
+        print(player1, player2, score1, score2)
+        currentWins = 0
+        currentLosses = 0
+
+        if score1 > score2:
+            winner = player1
+            loser = player2
+            currentWins += 1
+            currentLosses += 1
+        elif score1 < score2:
+            winner = player2
+            loser = player1
+            currentWins += 1
+            currentLosses += 1
+
+        updatedScores = {winner: currentWins, loser: currentLosses}
+        print(updatedScores)
+    
         with open("highscore.bin", "ab") as file:
             pickle.dump(self.scores, file)
 
