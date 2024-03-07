232ad49cd286c8086dd7bc2af6a2732e9985c599
Frida Johannesson <fridasanneamanda@gmail.com>
2024-03-07 22:08:39 +0100

    Edited the TestRules.py

--- src/Pig/Rules.py
+++ src/Pig/Rules.py
@@ -10,14 +10,9 @@ class Rules:
 
     def showRules(self):
         """Show the rules of the game."""
-        rules_text = """
-        >> Pig is a simple dice game where the goal is to reach 100 points first. <<
-        Players take turns rolling a single six-sided die: 
-        If the player rolls a 1, they score no points and lose their turn.
-        If the player rolls any other number, it is added to their turn total and their turn continues.
-        Players can choose to "hold", which adds their turn total to their score and ends their turn.
-        The first player to reach or exceed 100 points wins the game.
-        """
+        rules_text = "rules.txt"
+        with open(rules_text, "r") as file:
+            rules_text = file.read()
         return rules_text
 
     def __str__(self):
