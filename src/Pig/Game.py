c0063add8524d29e7d6ec4c984a46b71d8956f65
Merjam Farj Al-Beibani0015 <merjam.al-beibani0015@stud.hkr.se>
2024-03-08 13:52:32 +0100

    trying

--- src/Pig/Game.py
+++ src/Pig/Game.py
@@ -22,7 +22,6 @@ class Game:
 
     def player_vs_player(self, player1, player2):
         """Start the game. Player vs Player."""
-<<<<<<< HEAD
         self.players[player1] = 0
         self.players[player2] = 0
         self.scores = self.players
@@ -31,29 +30,25 @@ class Game:
         currentPlayer = player1
         dice = Dice(6)
  
-=======
-        current_player = player1
->>>>>>> c65fa58fbfc7384a5fcdc3969564bf333c76ebb0
         while True:
             print(">> Player vs Player <<\n")
             print(">> A new round of Pig is starting! <<")
             print(f"{player1.get_name()} has {player1.get_score()} points.")
             print(f"{player2.get_name()} has {player2.get_score()} points.")
 
-<<<<<<< HEAD
-                if result != 1:
-                    self.players[currentPlayer] += points_from_new_round
-                    print(f"Total points: {self.scores[currentPlayer]}")
-                    if self.players[currentPlayer] >= 6:
+            if result != 1:
+                self.players[currentPlayer] += points_from_new_round
+                print(f"Total points: {self.scores[currentPlayer]}")
+                if self.players[currentPlayer] >= 6:
                         print(f"Congratulations! {currentPlayer.name} wins!")
                         returnedDict = {key.name: value for key, value in self.players.items()}
                         for key, value in self.players.items():
                             print(f"{key.name} got {value} points.")
                         return returnedDict
-                    elif self.players[currentPlayer] < 6:
+                elif self.players[currentPlayer] < 6:
                         roll_again = input("Do you want to roll again? Enter yes or no: ").lower()
-                    print("")
-                    if roll_again == "no":
+                        print("")
+                if roll_again == "no":
                         print(f"Your turn is over. You got {self.scores[currentPlayer]} points.")
                         currentPlayer.score = self.players[currentPlayer]
                         currentPlayer = player2 if currentPlayer == player1 else player1
@@ -97,34 +92,3 @@ class Game:
                     self.players[currentPlayer] += 0
                     currentPlayer = intelligence if currentPlayer == player1 else player1
          
-=======
-            print(f"{current_player.get_name()} is playing.")
-            roll_the_dice = input("Press enter to roll the dice. ")
-            if roll_the_dice == "":
-                roll = self.dice.roll_the_dice()
-                tot = self.dice.show_the_dice(roll)
-                if tot == 0:
-                    current_player.reset_score()
-                    if current_player == player1:
-                        current_player = player2             
-                    else:
-                        current_player = player1
-                elif tot > 1:
-                    current_player.add_score(tot)
-                    if current_player.get_score() >= 6:
-                        print(f"{current_player.get_name()} has won the game!")
-                        return current_player.get_name() 
-                    print(f"{current_player.get_name()} has {current_player.get_score()} points.")
-                    hold = input("Do you want to hold? Yes or no: ")
-                    if hold.lower() == "yes":
-                        if current_player == player1:
-                            current_player = player2
-                        else:
-                            current_player = player1
-                    elif hold.lower() == "no":
-                        continue
-                    else:
-                        print("Invalid input. Press enter to roll the dice.")
-            else:
-                print("Invalid input. Press enter to roll the dice.")
->>>>>>> c65fa58fbfc7384a5fcdc3969564bf333c76ebb0
--- src/Pig/main.py
+++ src/Pig/main.py
@@ -1,34 +1,31 @@
-"""Main module for the Pig game."""
- 
-from Intelligence import Intelligence
-from Player import Player
+import random
 from Game import Game
+from Player import Player
 from Rules import Rules
 from HighScore import HighScore
+from Intelligence import Intelligence
 import sys
 
 
 
-def main():
-    """Main function for the Pig game."""
 
-   
-       
+
+def main():
     while True:
-<<<<<<< HEAD
-            print("Hello! Welcome to a game of Pig!")
-            print("*************************")
-            print("* 1. Player vs Computer *")
-            print("* 2. Player vs Player   *")
-            print("* 3. Highscore          *")
-            print("* 4. Rules              *")
-            print("* 5. Quit               *")
-            print("*************************")
+        """Main function for the Pig game."""
+        print("Hello! Welcome to a game of Pig!")
+        print("*************************")
+        print("* 1. Player vs Computer *")
+        print("* 2. Player vs Player   *")
+        print("* 3. Highscore          *")
+        print("* 4. Rules              *")
+        print("* 5. Quit               *")
+        print("*************************")
             
-            try:
-                option = int(input("Please enter an option: "))
+        try:
+            option = int(input("Please enter an option: "))
     
-                if option == 1:
+            if option == 1:
                     print(">> Player vs Computer <<\n")
                     intelligence = Intelligence()
                     player1 = Player(input("Please enter your name: "))
@@ -38,7 +35,7 @@ def main():
                      # Player method2
                     infoReturned = game.PlayerVsComputer(player1, intelligence)
 
-                elif option == 2:
+            elif option == 2:
                     print(">> Player vs Player <<\n")
                     playersName1 = input("Please enter your name (Player 1): ")
                     player1 = Player(playersName1)
@@ -47,8 +44,8 @@ def main():
                     newGame = Game()
                     infoReturned = newGame.PlayerVsPlayer(player1, player2)
             
-                elif option == 3:
-                    try:
+            elif option == 3:
+                try:
                         print(">> Highscore <<\n")
                         highScore = HighScore()
                         highScore.saveScore(infoReturned)
@@ -57,117 +54,33 @@ def main():
                         backToTheMenu = int(input("Enter 0 to go back to the menu: "))
                         if backToTheMenu == 0:
                             print("Back to the menu.")
-                    except UnboundLocalError:
+                except UnboundLocalError:
                         print("-------------------------")
                         print("No high score to show.")
                         print("Please play a game first.")
                         print("-------------------------")
-=======
-        print("Hello! Welcome to a game of Pig!")
-        print("*************************")
-        print("* 1. Player vs Computer *")
-        print("* 2. Player vs Player   *")
-        print("* 3. Highscore          *")
-        print("* 4. Rules              *")
-        print("* 5. Quit               *")
-        print("*************************")
-
-        try:
-            option = int(input("Please enter an option: "))
-
-            if option == 1:
-                print(">> Player vs Computer <<\n")
-                intelligence = Intelligence()
-                game = Game()
-                # Computer method
-                intelligence.intelligence_move()
-                # Player method2
-
-            elif option == 2:
-                print(">> Player vs Player <<\n")
-                print(">> Player 1 <<")
-                name1 = input("Enter your name: ")
-                if name1 == "":
-                    print("You must enter a name.")                      
-                else:
-                    change_name = input("Are you sure you want to use "
-                                        + "this name? Yes or no: ")
-                    if change_name.lower() == "yes":
-                        player1 = Player(name1, 0)
-                        print(f"Welcome {name1}!")
-                    elif change_name.lower() == "no":
-                        print("Please enter a new name.")
-                    else:
-                        print("Invalid input. Please enter 'yes' or 'no'.")
-                        break
-
-                print(">> Player 2 <<")
-                name2 = input("Enter your name: ")
-                if name2 == "":
-                    print("You must enter a name.")
-                else:
-                    change_name = input("Are you sure you want to use "
-                                        + "this name? Yes or no: ")
-                    if change_name.lower() == "yes":
-                        player2 = Player(name2, 0)
-                        print(f"Welcome {name2}!")
-                    elif change_name.lower() == "no":
-                        continue
-                    else:
-                        print("Invalid input. Please enter 'yes' or 'no'.")
-                        break
-                game = Game()
-               
-                winner = game.player_vs_player(player1, player2)
-
-            elif option == 3:
-                try:
-                    print(">> Highscore <<\n")
-                    highscore = HighScore()
-                    highscore.save_score(winner)
-                    show_score = highscore.load_score()
-                    print(show_score)
-                    highscore.get_highScore()
-                    show_score2 = highscore.get_highScore()
-                    print(show_score2)
-                    back_to_the_menu = int(input("Enter 0 to go back to the menu: "))
-                    if back_to_the_menu == 0:
-                        print("Back to the menu.")
-
-                except UnboundLocalError:
-                    print("-------------------------")
-                    print("No high score to show.")
-                    print("Please play a game first.")
-                    print("-------------------------")
 
             elif option == 4:
-                print(">> Rules <<\n")
-                the_rules = Rules("Rules of Pig")
-                print(the_rules.show_rules())
-                back_to_the_menu = int(input("Enter 0 to go back to the menu: "))
-                if back_to_the_menu == 0:
-                    print("Back to the menu.")
->>>>>>> c65fa58fbfc7384a5fcdc3969564bf333c76ebb0
-
-                elif option == 4:
                     print(">> Rules <<\n")
                     theRules = Rules("Rules of Pig")
                     print(theRules.showRules())
 
-                elif option == 5:
+            elif option == 5:
                     print("Goodbye!")
                     sys.exit()
 
-                else:
+            else:
                     print("---------------------------------")
                     print("Invalid option. Please try again.")
                     print("---------------------------------")
 
-            except ValueError:
+                
+        except ValueError:
                 print("------------------------------------------")
                 print("Invalid input. Please enter a option 1-5.")
                 print("------------------------------------------")
-
+                
 
 if __name__ == "__main__":
     main()
+    
