14dfe13834a7d03834883361ab12cf8dbc1c77dc
Frida Johannesson <fridasanneamanda@gmail.com>
2024-03-08 09:15:29 +0100

    Editing the game class

--- src/Pig/Game.py
+++ src/Pig/Game.py
@@ -15,38 +15,3 @@ class Game:
 
     def PlayerVsPlayer(self, player1, player2):
         """Start the game. Player vs Player."""
-        self.players[player1] = 0
-        self.players[player2] = 0
-        self.scores = self.players
-
-        print(">> Player vs Player <<\n")
-        currentPlayer = player1
-        dice = Dice(6)
-
-        while True:
-            roll_the_dice = input(f"Players {currentPlayer.name} turn "
-                                  + "- please enter 0 to roll the dice: ")
-            if roll_the_dice == "0":
-                result = dice.rollTheDice()
-                points_from_new_round = dice.showTheDice(result)
-
-                if result != 1:
-                    self.players[currentPlayer] += points_from_new_round
-                    print(f"Total points: {self.scores[currentPlayer]}")
-                    if self.players[currentPlayer] >= 6:
-                        print(f"Congratulations! {currentPlayer.name} wins!")
-                        returnedDict = {key.name: value for key, value in self.players.items()}
-                        for key, value in self.players.items():
-                            print(f"{key.name} got {value} points.")
-                        return returnedDict
-                    elif self.players[currentPlayer] < 6:
-                        roll_again = input("Do you want to roll again? Enter yes or no: ").lower()
-                    print("")
-                    if roll_again == "no":
-                        print(f"Your turn is over. You got {self.scores[currentPlayer]} points.")
-                        currentPlayer.score = self.players[currentPlayer]
-                        currentPlayer = player2 if currentPlayer == player1 else player1
-                else:
-                    self.players[currentPlayer] += 0
-                    currentPlayer = player2 if currentPlayer == player1 else player1
-         
--- src/Pig/HighScore.py
+++ src/Pig/HighScore.py
@@ -15,24 +15,6 @@ class HighScore:
 
     def saveScore(self, dictOfScores):
         """Save the high score to a file."""
-<<<<<<< HEAD
-        updatedScores = {}
-        score1, score2 = dictOfScores.values()
-        player1, player2 = dictOfScores.keys()
-
-        print(score1, player1, score2, player2)
-        if score1 > score2:
-            updatedScores.update(player1)
-            for key, value in updatedScores.items():
-                print(f"{key} got {value} points.")
-            print(f"Winner: {player1}")
-            self.scores[player1] = score1
-            updatedScores.update(player1)
-        elif score1 < score2:
-            print(f"Winner: {player2}")
-            self.scores[player2] = score2
-            updatedScores.update(self.scores)
-=======
         highScoreDict = dictOfScores
         player1, player2 = list(highScoreDict.keys())
         score1, score2 = list(highScoreDict.values())
@@ -53,7 +35,6 @@ class HighScore:
         updated_Scores = {winner: win, loser: loss}
         self.scores.update(updated_Scores)
         print(updated_Scores)
->>>>>>> main
 
         with open("highscore.bin", "ab") as file:
             pickle.dump(self.scores, file)
