bdfe07e85043f62264b7858b1aa149ed532fcda3
Frida Johannesson <fridasanneamanda@gmail.com>
2024-03-13 11:10:30 +0100

    OK

--- src/Pig/Game.py
+++ src/Pig/Game.py
@@ -17,7 +17,8 @@ class Game:
     def __str__(self):
         """Return the result of the game."""
         return "Result: Players"
-        
+
+  
     def player_vs_computer(self, player1, intelligence):
         """Start the game. Player vs Computer."""
         self.players[current_player] = 0
@@ -57,8 +58,7 @@ class Game:
     def player_vs_player(self, player1, player2):
         """Start the game. Player vs Player."""
         current_player = player1
-        rolling_total = []
-            
+        rolling_total = []         
         while True:
             print(">> Player vs Player <<\n")
             print("")
@@ -71,9 +71,8 @@ class Game:
 
             roll_the_dice = input("Press enter to roll the dice. ")
             if roll_the_dice == "":
-                roll = self.dice.roll_the_dice()  
+                roll = self.dice.roll_the_dice()
                 tot = self.dice.show_the_dice(roll)
-               
                 if roll != 1:
                     print(f"You rolled a {roll}.")
                     rolled = 0
@@ -88,12 +87,13 @@ class Game:
                     rolling_total.clear()
                     if current_player == player1:
                         current_player = player2
-                           
-                        print(f"{player1.get_name()} has {player1.get_score()} points.")
-                        print(f"{player2.get_name()} has {player2.get_score()} points.\n")
+                        print(f"{player1.get_name()} "
+                              + f"has {player1.get_score()} points.")
+                        print(f"{player2.get_name()} "
+                              + f"has {player2.get_score()} points.\n")
                     else:
                         rolling_total.clear()
-                        current_player = player1                  
+                        current_player = player1
                         print(f"{player1.get_name()} has {player1.get_score()} points.")
                         print(f"{player2.get_name()} has {player2.get_score()} points.\n")
 
@@ -108,11 +108,9 @@ class Game:
                         rolling_total.clear()
                         if current_player == player1:
                             current_player = player2
-                          
                         else:
                             rolling_total.clear()
                             current_player = player1
-                        
                     elif hold.lower() == "no":
                         continue
                     else:
